903df36f75aed9c4a1541bb511e91eb7f1a7ceb0
phucha1986 <56842584+phucha1986@users.noreply.github.com>
2019-10-22 13:08:35 +0400

    Update and rename auth0-variables.example.js to auth0-variables.js

--- src/Auth/auth0-variables.example.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-  domain: 'XXXXXXXXXX.auth0.com',
-  clientID: 'XXXXXXXX',
-  callbackUrl: 'http://localhost:1234/callback'
-};
---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'dev-4h9w8mr6.auth0.com',
+  clientID: 'Cg1o4j_G80gREq2DPyMn5CIcvPtBaXmON23EItlBKrKT8NYd1rxrPdKYM11dn2w1',
+  callbackUrl: 'http://localhost:3000/callback'
+};
